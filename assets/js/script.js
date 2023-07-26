1b0af072a9ef75ef497c5fb34368584f5e476af0
Owen Lang <owen.lang146@gmail.com>
2023-07-26 16:12:12 -0500

    html and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
 var apiKey = 'bcca9cde2077618db7f70b7605e37960';
+
+var searchBtn = $('#search')
+var pastCities = $('#past-cities')
+var searchBox = $('#city-form')
+
+function searchCities(evt) {
+    evt.preventDefault();
+
+    var city = searchBox.val();
+    
+    if (!city) {
+        return;
+    }
+    console.log('a;lkjgf')
+}
+
+searchBtn.on('click', searchCities);
